43f3c8e3eac41203ea4cdb82b26c549d7360b208
Éber Mainardes <mainardes.eber@gmail.com>
2020-04-19 14:32:22 -0300

    Using the correct validator when creating a category

--- src/routes/categories/index.js
+++ src/routes/categories/index.js
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 
 import CategoryController from '../../app/controllers/CategoryController';
-import { updateValidator } from '../../app/validators/Category';
+import { storeValidator, updateValidator } from '../../app/validators/Category';
 
 const routes = new Router();
 
-routes.post('/', updateValidator, CategoryController.store);
+routes.post('/', storeValidator, CategoryController.store);
 routes.put('/', updateValidator, CategoryController.update);
 
 export default routes;
