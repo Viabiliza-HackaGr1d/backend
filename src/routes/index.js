ee9342d67653c1baf0f7606adbbb745584d70ef4
Éber Mainardes <mainardes.eber@gmail.com>
2020-04-19 01:20:45 -0300

    Adding categories routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,13 @@ import { Router } from 'express';
 
 import customers from './customers';
 import brokers from './brokers';
+import categories from './categories';
 
 const routes = new Router();
 
 routes.use('/customers', customers);
 routes.use('/brokers', brokers);
+
+routes.use('/categories', categories);
 
 export default routes;
